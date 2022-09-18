dade94a1531ec35a2b8f089036b1fbe5ab4577ee
salwaasalsabilla <113820985+salwaasalsabilla@users.noreply.github.com>
2022-09-18 14:01:12 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,110 @@
+'<!DOCTYPE html>'
+<html>
+    <head>
+        <title>Contoh HTML</title>
+    </head>
+    <body>
+
+      <!--Contoh Paragraf-->
+         <p align="center">ini script HTML pertama. ini script HTML pertama.ini script HTML pertama. ini script HTML pertama.
+         ini script HTML pertama. ini script HTML pertama.ini script HTML pertama. ini script HTML pertama.
+         ini script HTML pertama. ini script HTML pertama.ini script HTML pertama. ini script HTML pertama.
+         <p align "right">ini script HTML pertama.ini script HTML pertama. ini script HTML pertama.ini script HTML pertama.
+         ini script HTML pertama. ini script HTML pertama.ini script HTML pertama. ini script HTML pertama.
+         ini script HTML pertama. ini script HTML pertama.ini script HTML pertama. ini script HTML pertama.
+         ini script HTML pertama. ini script HTML pertama.ini script HTML pertama. ini script HTML pertama.
+
+      <!--Contoh image-->
+      <h2>Contoh image HTML5</h2>
+         <img src="udinus.jpg"
+
+      <!--Contoh table-->
+      <h2>Contoh image HTML 5 </h2>
+          <table border="1" width="50%">
+             <tr>
+                 <td colspan="2">1</td>
+                 <td>2</td>
+                 <td>3</td>
+              </tr>
+
+              <tr>
+                  <td rowspan="2"> 4 7 </td
+                  <td>5</td>
+                  <td>6</td>
+              </tr>
+
+              <tr>
+                  <td>8</td>
+                  <td>9</td>
+
+           </table>
+           </body>
+
+    <!--contohlists-->
+    <h2>Contoh List HTML 5</h2>
+        <o1>
+        <body>
+           <li> Fakuoltas Ilmu Komputer</li>
+           <li>Fakultas Ekonomi dan Bisnis</li>
+           <li>Fakultas Kesehatan</li>
+           <li>Fakultas Teknik</li>
+           <li>Ilmu Budaya</li>
+        </o1>
+        </body>
+
+    <--Contoh lists kedua-->
+    <h2>Contoh 2 list HTML5 (unorder list)</h2>
+       <ul type "A">
+       </body>
+
+    <!--Contoh lists kedua-->
+    <h2>contoh 2 list HTML 5 (unorder lists)
+        <ul type "A">
+        <body>
+              <li>Fakultas Ilmu Komputer</li>
+              <li>Fakultas Ekonomi dan Bisnis</li>
+        <ul type "B">
+              <li>Fakultas Ilmu Budaya</li>
+              <li>Fakultas Tehnik</li>
+        </ul>
+        </body>
+
+    <!--Contoh Links-->
+    <h2>Contoh link HTML 5</h2>
+        <a href="https://www.tokopedia.com/vortexseries/vortexseries-gaming-mouse-gx770-hitam?ref=wishlist" mouse gaming</a>
+
+
+    <!--contoh heading-->
+    <h1>contoh Heading HTML</h1>
+        <h2><b>-ini CONTOH TUGAS SY-</h2>
+        <h2>Udinus</h2>
+        <h3>Fakultas Ilmu Komputer</h2>
+        <h4>Tehnik Informatika</h4>
+        <h5>Salwa Salsabilla</h5>
+        <h6>NIM A11202214187</h6>
+
+    <!--Contoh Text Formating-->
+    <h1>Contoh Text Formating</h1>
+        <b>- Bold text </b>
+        <br>
+        <strong>- Imporant text</i>
+        <br>
+        <i>- Italic text </i>
+        <br>
+        <em>- Emphasized text</em>
+        <br>
+        <mark>- Marked text</mark>
+        <br>
+        <small>- Smaller text</small>
+        <br>
+        <del>- Delete text</del>
+        <br>
+        <ins>- Inserted text</ins>
+        <br>
+        <sub>- Subscript text</sub>
+        <br>
+        <sup>- Superscript text</sup>
+
+   <!--Contoh Coments-->
+      <h1 align="center">Contoh Coments</h1>
+      <p align="center">HTML atau Hypertext Markup Language merupakan sebuah bahasa markah untuk membuat halaman web</p>
